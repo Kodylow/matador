fdad2265aeeaf09958f9995321f627cd7e8b28fb
kody.low <kodylow7@gmail.com>
2023-08-20 21:13:13 -0700

    checkpoint before adding streaming

--- src/services/openai/types.rs
+++ src/services/openai/types.rs
@@ -1,10 +1,35 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct ChatCompletionRequest {
     model: String,
     messages: Vec<Message>,
-    // Add other fields as per your requirements
+    functions: Option<Vec<Function>>,
+    function_call: Option<FunctionCall>,
+    temperature: Option<f32>,
+    top_p: Option<f32>,
+    n: Option<i32>,
+    pub stream: Option<bool>,
+    stop: Option<Vec<String>>,
+    max_tokens: Option<i32>,
+    presence_penalty: Option<f32>,
+    frequency_penalty: Option<f32>,
+    logit_bias: Option<HashMap<String, f32>>,
+    user: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Function {
+    name: String,
+    description: Option<String>,
+    parameters: serde_json::Value,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct FunctionCall {
+    name: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -34,5 +59,26 @@ pub struct Usage {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Message {
     role: String,
+    content: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ChatCompletionChunk {
+    id: String,
+    object: String,
+    created: i64,
+    model: String,
+    choices: Vec<ChoiceChunk>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ChoiceChunk {
+    index: i32,
+    delta: Delta,
+    finish_reason: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Delta {
     content: String,
 }
