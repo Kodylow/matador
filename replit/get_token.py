5cffe00ca78922eff0ab164371068919d2654538
kody.low <kodylow7@gmail.com>
2023-10-01 01:52:31 +0000

    update

--- replit/get_token.py
+++ replit/get_token.py
@@ -4,6 +4,7 @@ import json
 token_manager = ReplitIdentityTokenManager()
 token = token_manager.get_token()
 timeout = token_manager.get_token_timeout()
+
 print(json.dumps({
     "token": token,
     "timeout": timeout,
