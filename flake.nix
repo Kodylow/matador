2bcf541da1bb9136d4e028ac4d36d8ec6abbbf2a
kody.low <kodylow7@gmail.com>
2023-10-02 16:02:41 -0700

    chore: add flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Matador: A Bitcoin Payments Reverse Proxy for any API";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+
+    flakebox = {
+      url = "github:rustshop/flakebox";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flakebox, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        flakeboxLib = flakebox.lib.${system} { };
+      in
+      {
+        devShells = {
+          default = flakeboxLib.mkDevShell {
+            packages = [ ];
+          };
+        };
+      });
+}
