9add93a9e790d8777c06cd9f6c2a8f7ef49ab16f
kody.low <kodylow7@gmail.com>
2023-10-06 11:17:33 +0200

    feat: nix flake updates

--- flake.nix
+++ flake.nix
@@ -2,21 +2,25 @@
   description = "Matador: A Bitcoin Payments Reverse Proxy for any API";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flakebox = {
-      url = "github:rustshop/flakebox?rev=389987aadbc291d0dff842b898b643d5e6a8d140";
+      url =
+        "github:rustshop/flakebox?rev=389987aadbc291d0dff842b898b643d5e6a8d140";
     };
-
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flakebox, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+        pkgs = import nixpkgs { inherit system; };
+
+        isDarwin = system == "x86_64-darwin" || system == "aarch64-darwin";
+        darwinFrameworks = with pkgs.darwin.apple_sdk.frameworks; [
+          SystemConfiguration
+          CoreFoundation
+          Security
+        ];
 
         flakeboxLib = flakebox.lib.${system} { };
 
@@ -25,47 +29,39 @@
             name = "matador";
             path = ./.;
           };
-          dirs = [
-            "Cargo.toml"
-            "Cargo.lock"
-            ".cargo"
-            "src"
-          ];
+          dirs = [ "Cargo.toml" "Cargo.lock" ".cargo" "src" ];
         };
 
-        multibuild =
-          (flakeboxLib.craneMultiBuild { }) (craneLib':
-            let
-              craneLib = (craneLib'.overrideArgs {
-                pname = "flexbox-multibuild";
-                src = rustSrc;
-                buildInputs = [
-                  pkgs.openssl
-                ];
-
-                nativeBuildInputs = [
-                  pkgs.pkg-config
-                ];
-              });
-            in
-            rec {
-              workspaceDeps = craneLib.buildWorkspaceDepsOnly { };
-              workspaceBuild = craneLib.buildWorkspace {
-                cargoArtifacts = workspaceDeps;
-              };
-              matador = craneLib.buildPackage { };
+        multibuild = (flakeboxLib.craneMultiBuild { }) (craneLib':
+          let
+            craneLib = (craneLib'.overrideArgs {
+              pname = "flexbox-multibuild";
+              src = rustSrc;
+              buildInputs = [ pkgs.openssl ]
+                ++ (if isDarwin then darwinFrameworks else [ ]);
+              propagatedBuildInputs =
+                if isDarwin then darwinFrameworks else [ ];
+              nativeBuildInputs = [ pkgs.pkg-config ];
+              PKG_CONFIG_PATH = if isDarwin then
+                "${
+                  pkgs.lib.getDev
+                  pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+                }/lib/pkgconfig"
+              else
+                null;
             });
-       in
-       {
+          in rec {
+            workspaceDeps = craneLib.buildWorkspaceDepsOnly { };
+            workspaceBuild =
+              craneLib.buildWorkspace { cargoArtifacts = workspaceDeps; };
+            matador = craneLib.buildPackage { };
+          });
+      in {
         legacyPackages = multibuild;
         devShells = {
           default = flakeboxLib.mkDevShell {
-            buildInputs = [
-              pkgs.openssl
-            ];
-            nativeBuildInputs = [
-              pkgs.pkg-config
-            ];
+            buildInputs = [ pkgs.openssl ];
+            nativeBuildInputs = [ pkgs.pkg-config ];
             packages = [ ];
           };
         };
