c269df6c8b7d0f80c2cd8bd5c0f24aaa7c769968
kody.low <kodylow7@gmail.com>
2023-10-08 13:56:43 +0200

    remove fancy nix stuff for now

--- src/web/mw/mw_l402.rs
+++ src/web/mw/mw_l402.rs
@@ -1,15 +1,34 @@
 use axum::http::{HeaderValue, Request, StatusCode};
 use axum::middleware::Next;
 use axum::response::{IntoResponse, Response};
+use moksha_core::*;
 
 use super::error::Result;
 use crate::lightning::l402::L402;
-use crate::lightning::L402Builder;
+use crate::lightning::{L402Builder, LightningAddress};
 
 const WWW_AUTHENTICATE: &str = "www-authenticate";
 
 pub async fn mw_l402<B>(req: Request<B>, next: Next<B>) -> Result<Response> {
     let headers = req.headers();
+
+    // Check if the x-cashu header is present
+    let header = headers.get("X-Cashu").or(headers.get("x-cashu"));
+
+    if header.is_some() {
+        if handle_cashu(header.unwrap()) {
+            return Ok(next.run(req).await);
+        } else {
+            let mut res = StatusCode::PAYMENT_REQUIRED.into_response();
+            let l402 = L402Builder::new().build().await?;
+            res.headers_mut().insert(
+                WWW_AUTHENTICATE,
+                HeaderValue::from_str(&l402.to_authenticate_string()).unwrap(),
+            );
+
+            return Ok(res);
+        }
+    }
 
     // Check if the authorization header is present, handle Authorization and
     // authorization
@@ -47,4 +66,28 @@ pub async fn mw_l402<B>(req: Request<B>, next: Next<B>) -> Result<Response> {
             Ok(res)
         }
     }
+}
+
+async fn handle_cashu(header: &HeaderValue) -> bool {
+    let header = header.to_str().unwrap().to_string();
+    let tokens = TokenV3::deserialize(header).unwrap();
+    let lnaddress = LightningAddress::new(
+        dotenv::var("LNADDRESS")
+            .expect("LNADDRESS not set")
+            .as_str(),
+    );
+    if tokens.total_amount() < 1 {
+        return false;
+    }
+    for token in tokens {
+        let melt_url = format!("{}/melt", token.mint_url());
+        let invoice = lnaddress.get_invoice(token.total_amount()).await;
+        let pr = invoice.
+    }
+
+    if header == "cashu" {
+        return true;
+    }
+
+    false
 }
